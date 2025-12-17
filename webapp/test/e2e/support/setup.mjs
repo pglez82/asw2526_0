93eab34dd36179e5e6aa86bfbde416452278f653
pglez82 <pglez82@gmail.com>
2025-12-17 10:32:39 +0100

    refactoring setup of e2e

--- webapp/test/e2e/support/setup.mjs
+++ webapp/test/e2e/support/setup.mjs
@@ -12,9 +12,9 @@ setWorldConstructor(CustomWorld)
 
 Before(async function () {
   // Allow turning off headless mode and enabling slow motion/devtools via env vars
-  const headless = process.env.PLAYWRIGHT_HEADLESS !== 'false'
-  const slowMo = process.env.PLAYWRIGHT_SLOWMO ? Number(process.env.PLAYWRIGHT_SLOWMO) : 0
-  const devtools = process.env.PLAYWRIGHT_DEVTOOLS === 'true'
+  const headless = true
+  const slowMo = 0
+  const devtools = false
 
   this.browser = await chromium.launch({ headless, slowMo, devtools })
   this.page = await this.browser.newPage()
